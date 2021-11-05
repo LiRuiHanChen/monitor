189d5f11264027c9bfa9d87f7270950c6534a1b7
李雷 <lei.li@17zuoye.com>
2021-11-05 16:31:06 +0800

    增加小学注册

--- src/main/java/com/monitor/argent/commons/StudentRequestUtil.java
+++ src/main/java/com/monitor/argent/commons/StudentRequestUtil.java
@@ -1,7 +1,9 @@
 package com.monitor.argent.commons;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.monitor.argent.api.Code;
+import com.monitor.argent.entity.StudentRegisterBody;
 import com.monitor.argent.entity.StudentRegisterRequestBody;
 import com.monitor.argent.model.Result;
 import org.apache.commons.beanutils.BeanUtils;
@@ -27,10 +29,19 @@ public class StudentRequestUtil {
     private static final String MOBILE_BINDING_CHECK = "/v1/student/mobilebindingcheck.vpage";
     private static final String USER_REGISTER = "/v1/user/register.api";
     private static final String GET_REGISTER_CODE = "/v1/user/register/verifycode/get.api";
+    //    private static final String SIGNUP_CHECK_CLAZZ_INFO = "/signup/checkclazzinfo.api";
+//    private static final String SIGNUP_FILTER_NAME = "/signup/filtersensitiveusername.api";
+    private static final String SIGNUP_SVC_API = "/signup/smsignsvc.api";
+    private static final String SIGNUP_SIGNUP_API = "/signup/signup.api";
     // 测试环境
     public static final String URL_PATH = "https://api.test.17zuoye.net";
+    public static final String SIGNUP_URL_PATH = "https://ucenter.test.17zuoye.net";
     public static final String APP_KEY = "17JuniorStu";
+    // 专用小学key
+    public static final String SIGNUP_APP_KEY = "17Student";
     public static final String SECRET_KEY = "NzVe9rUVkWQt";
+    // 专用小学key
+    public static final String SIGNUP_SECRET_KEY = "kuLwGZMJBcQj";
 
     @Resource
     HttpRequestUtil httpRequestUtil;
@@ -60,7 +71,7 @@ public class StudentRequestUtil {
         }
         paramMap.put("sig", sig);
         // 请求
-        return getObjectResult(paramMap, CHECK_CLAZZ_INFO);
+        return getObjectResult(paramMap, URL_PATH, CHECK_CLAZZ_INFO);
     }
 
     /**
@@ -85,7 +96,7 @@ public class StudentRequestUtil {
             e.printStackTrace();
         }
         // 请求
-        return getObjectResult(paramMap, CHECK_KLX_STUDENT_REGISTER_INFO);
+        return getObjectResult(paramMap, URL_PATH, CHECK_KLX_STUDENT_REGISTER_INFO);
     }
 
     /**
@@ -107,7 +118,7 @@ public class StudentRequestUtil {
             e.printStackTrace();
         }
         // 请求
-        return getObjectResult(paramMap, CHECK_DUPLICATE_NAME);
+        return getObjectResult(paramMap, URL_PATH, CHECK_DUPLICATE_NAME);
     }
 
     /**
@@ -132,7 +143,7 @@ public class StudentRequestUtil {
             e.printStackTrace();
         }
         // 请求
-        return getObjectResult(paramMap, MOBILE_BINDING_CHECK);
+        return getObjectResult(paramMap, URL_PATH, MOBILE_BINDING_CHECK);
     }
 
     /**
@@ -151,7 +162,7 @@ public class StudentRequestUtil {
             e.printStackTrace();
         }
         // 请求
-        return getObjectResult(paramMap, GET_REGISTER_CODE);
+        return getObjectResult(paramMap, URL_PATH, GET_REGISTER_CODE);
     }
 
     /**
@@ -170,7 +181,7 @@ public class StudentRequestUtil {
             e.printStackTrace();
         }
         // 请求
-        return getObjectResult(paramMap, USER_REGISTER);
+        return getObjectResult(paramMap, URL_PATH, USER_REGISTER);
     }
 
     /**
@@ -192,7 +203,7 @@ public class StudentRequestUtil {
             e.printStackTrace();
         }
         // 请求
-        return getObjectResult(map, JOIN_SYSTEM_CLAZZ);
+        return getObjectResult(map, URL_PATH, JOIN_SYSTEM_CLAZZ);
     }
 
     /**
@@ -202,11 +213,11 @@ public class StudentRequestUtil {
      * @param requestPathInfo
      * @return
      */
-    private Result<Object> getObjectResult(Map<String, String> paramMap, String requestPathInfo) {
+    private Result<Object> getObjectResult(Map<String, String> paramMap, String urlPath, String requestPathInfo) {
         try {
             HashMap<String, String> headerMap = new HashMap<>();
             headerMap.put("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36");
-            Map<String, String> resultMap = httpRequestUtil.sendPostDataByMap(URL_PATH + requestPathInfo, headerMap, paramMap);
+            Map<String, String> resultMap = httpRequestUtil.sendPostDataByMap(urlPath + requestPathInfo, headerMap, paramMap);
             if (!resultMap.isEmpty()) {
                 if (resultMap.get("code").equals("200")) {
                     return Result.success(JSONObject.parseObject(resultMap.get("response")));
@@ -230,6 +241,10 @@ public class StudentRequestUtil {
      * @throws NoSuchMethodException
      */
     public Map<String, String> parseBeanToMap(StudentRegisterRequestBody studentRegisterBody) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+        return BeanUtils.describe(studentRegisterBody);
+    }
+
+    public Map<String, String> parseSignupBeanToMap(StudentRegisterBody studentRegisterBody) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
         return BeanUtils.describe(studentRegisterBody);
     }
 
@@ -270,12 +285,12 @@ public class StudentRequestUtil {
      *
      * @return
      */
-    public ArrayList<String> loadMobileList(int count) {
+    public ArrayList<String> loadMobileList(int count, String phoneNumber) {
         if (count == 0 || count >= 10) return null;
         ArrayList<String> list = new ArrayList<>(count);
         for (int i = 0; i < count; i++) {
             StringBuilder phoneLastNum = new StringBuilder();
-            phoneLastNum.append("179");//手机号标准格式
+            phoneLastNum.append(phoneNumber);//手机号标准格式
             for (int j = 0; j < 8; j++) {
                 //每次循环都从0~9挑选一个随机数
                 phoneLastNum.append((int) (Math.random() * 10));
@@ -284,4 +299,139 @@ public class StudentRequestUtil {
         }
         return list;
     }
+
+    /**
+     * 小学学生注册--检查老师ID
+     *
+     * @param id
+     * @return
+     */
+    public Result<Object> signupCheckClazzInfo(String id) {
+        //计算sig
+        Map<String, String> paramMap = new HashMap();
+        paramMap.put("app_key", SIGNUP_APP_KEY);
+        paramMap.put("id", id);
+        String sig = loadSigValue(SIGNUP_SECRET_KEY, paramMap);
+        if (StringUtils.isEmpty(sig)) {
+            return Result.failure(Code.BAD_REQUEST, "sig计算失败");
+        }
+        paramMap.put("sig", sig);
+        // 请求
+        return getObjectResult(paramMap, URL_PATH, CHECK_CLAZZ_INFO);
+    }
+
+    /**
+     * 小学端注册--检查姓名有效性
+     *
+     * @param name
+     * @return
+     */
+    public Result<Object> checkSignupRealName(String name, String clazzId, String teacherId) {
+        Map<String, String> paramMap = new HashMap<>();
+        paramMap.put("real_name", name);
+        paramMap.put("clazz_id", clazzId);
+        paramMap.put("teacher_id", teacherId);
+        paramMap.put("app_key", SIGNUP_APP_KEY);
+        try {
+            String sig = loadSigValue(SIGNUP_SECRET_KEY, paramMap);
+            if (!StringUtils.isEmpty(sig)) {
+                paramMap.put("sig", sig);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return getObjectResult(paramMap, URL_PATH, CHECK_DUPLICATE_NAME);
+    }
+
+    /**
+     * 小学学生--判断是否可以加入班级
+     *
+     * @return
+     */
+    public Result<Object> requestSignupJoinSystemClazz(String clazzId, String teacherId) {
+        Map<String, String> paramMap = new HashMap<>();
+        paramMap.put("clazz_id", clazzId);
+        paramMap.put("teacher_id", teacherId);
+        paramMap.put("app_key", SIGNUP_APP_KEY);
+        try {
+            String sig = loadSigValue(SIGNUP_SECRET_KEY, paramMap);
+            if (!StringUtils.isEmpty(sig)) {
+                paramMap.put("sig", sig);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return getObjectResult(paramMap, URL_PATH, JOIN_SYSTEM_CLAZZ);
+    }
+
+    /**
+     * 小学学生注册--检查手机号是否绑定
+     *
+     * @param phoneNumber
+     * @return
+     */
+    public Result<Object> requestSignupMobileCheck(String teacherId, String phoneNumber) {
+        Map<String, String> paramMap = new HashMap<>();
+        //增加user_code
+        paramMap.put("user_code", phoneNumber);
+        paramMap.put("teacher_id", teacherId);
+        String sig = loadSigValue(SIGNUP_SECRET_KEY, paramMap);
+        if (!StringUtils.isEmpty(sig)) {
+            paramMap.put("sig", sig);
+        }
+        // 请求
+        return getObjectResult(paramMap, URL_PATH, MOBILE_BINDING_CHECK);
+    }
+
+    /**
+     * 小学端注册--手机号发送验证码
+     */
+    public Result<Object> signupSmsignSvc(String mobile, String teacher_id) {
+        Map<String, String> paramMap = new HashMap();
+        paramMap.put("user_type", "3");
+        paramMap.put("user_code", mobile);
+        paramMap.put("teacher_id", teacher_id);
+        paramMap.put("imei", "ed942adf-047f-4101-a542-d4dfcf1cac9d");
+        paramMap.put("app_key", SIGNUP_APP_KEY);
+        String sig = loadSigValue(SIGNUP_SECRET_KEY, paramMap);
+        if (!StringUtils.isEmpty(sig)) {
+            paramMap.put("sig", sig);
+        }
+        // 请求
+        return getObjectResult(paramMap, URL_PATH, GET_REGISTER_CODE);
+    }
+
+    /**
+     * 小学注册---构造请求参数
+     *
+     * @param paramMap
+     * @param mobileNumber
+     * @param passwd
+     * @return
+     */
+    public Map<String, String>
+    signupRegisterParamToMap(Map<String, String> paramMap, String realName, String mobileNumber, String passwd) {
+        paramMap.remove("count");
+        paramMap.remove("teacher_id");
+        paramMap.put("passwd", passwd);
+        paramMap.put("verify_code", "1234");
+        paramMap.put("user_type", "3");
+        paramMap.put("nick_name", "");
+        paramMap.put("avatar_dat", "");
+        paramMap.put("_cid", "15-1-0");
+        paramMap.put("invitor_id", "");
+        paramMap.put("user_code", mobileNumber);
+        paramMap.put("app_key", SIGNUP_APP_KEY);
+        String sig = loadSigValue(SIGNUP_SECRET_KEY, paramMap);
+        if (!StringUtils.isEmpty(sig)) {
+            paramMap.put("sig", sig);
+        }
+        return paramMap;
+    }
+
+    public Result<Object> signupUser(Map<String, String> paramMap) {
+        return getObjectResult(paramMap, URL_PATH, USER_REGISTER);
+    }
+
+
 }
--- src/main/java/com/monitor/argent/controller/StudentController.java
+++ src/main/java/com/monitor/argent/controller/StudentController.java
@@ -7,6 +7,7 @@ import com.monitor.argent.entity.RegisterStudentResponse;
 import com.monitor.argent.entity.StudentRegisterBody;
 import com.monitor.argent.entity.StudentRegisterRequestBody;
 import com.monitor.argent.model.Result;
+import lombok.SneakyThrows;
 import org.apache.commons.beanutils.BeanUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
@@ -23,8 +24,16 @@ public class StudentController {
     @Resource
     StudentRequestUtil studentRequestUtil;
 
+    String imei = "EA22A997-9E02-4380-8783-D0FFCDCDCA3E";
+    String passwd = "test1234";
     private static final String[] numChars = {"壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖", "拾"};
 
+    /**
+     * 中学端学生注册--检查老师ID号有效性
+     *
+     * @param id 老师号
+     * @return
+     */
     @RequestMapping(value = "/check/teacher", method = RequestMethod.POST)
     @ResponseBody
     public Result<Object> checkTeacher(@RequestParam String id) {
@@ -33,6 +42,15 @@ public class StudentController {
         return studentRequestUtil.requestCheckClazzInfo(id);
     }
 
+    /**
+     * 中学端学生注册--手机号179xxxxxxxx
+     *
+     * @param studentRegisterBody
+     * @return
+     * @throws InvocationTargetException
+     * @throws IllegalAccessException
+     * @throws NoSuchMethodException
+     */
     @RequestMapping(value = "/register/student", method = RequestMethod.POST)
     @ResponseBody
     public Result<Object> register(@RequestBody StudentRegisterBody studentRegisterBody) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
@@ -44,12 +62,11 @@ public class StudentController {
         //定长list，来源于前端指定长度 <=10
         List<RegisterStudentResponse> registerStudentResponseList = new ArrayList<>(count);
         String sessionKey = null;
-        String imei = "EA22A997-9E02-4380-8783-D0FFCDCDCA3E";
-        String passwd = "test1234";
+
         //根据需要生成的账号（count数量）来遍历姓名：（我是学生一）
         String realName = studentRegisterBody.getReal_name();
         //获取随机手机号
-        ArrayList<String> mobileList = studentRequestUtil.loadMobileList(count);
+        ArrayList<String> mobileList = studentRequestUtil.loadMobileList(count, "179");
         for (int i = 0; i < count; i++) {
             //修改学生名字
             studentRegisterBody.setReal_name(realName + numChars[i]);
@@ -172,4 +189,101 @@ public class StudentController {
         return Result.success(registerStudentResponseList);
     }
 
+    /**
+     * 小学端学生注册，检查老师ID有效性
+     *
+     * @param id 老师ID
+     * @return
+     */
+    @RequestMapping(value = "/signup/check/teacher/info", method = RequestMethod.POST)
+    @ResponseBody
+    public Result<Object> checkClazzInfo(@RequestParam String id) {
+        if (StringUtils.isEmpty(id)) return Result.failure(Code.PARAMETER_MISSING, "参数异常");
+        //检查并判断老师ID
+        return studentRequestUtil.signupCheckClazzInfo(id);
+    }
+
+    /**
+     * 小学注册---手机号：171xxxxxx
+     *
+     * @param studentRegisterBody
+     * @return
+     */
+    @RequestMapping(value = "/signup/register", method = RequestMethod.POST)
+    @ResponseBody
+    public Result<Object> signupRegister(@RequestBody StudentRegisterBody studentRegisterBody) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+        if (studentRegisterBody == null) return Result.failure(Code.PARAMETER_MISSING, "参数异常");
+        long clazzId = studentRegisterBody.getClazz_id();
+        long teacherId = studentRegisterBody.getTeacher_id();
+        if (clazzId == 0 || teacherId == 0)
+            return Result.failure(Code.PARAMETER_MISSING, "参数异常");
+
+        if (StringUtils.isEmpty(studentRegisterBody.getReal_name()))
+            return Result.failure(Code.PARAMETER_MISSING, "姓名不能为空");
+
+        int count = studentRegisterBody.getCount();
+        if (count <= 0 || count > 10) return Result.failure(Code.BAD_REQUEST, "生成账号数量异常");
+
+        //定长list，来源于前端指定长度 <=10
+        List<RegisterStudentResponse> registerStudentResponseList = new ArrayList<>(count);
+
+        /*
+         根据需要生成的账号（count数量）来遍历姓名：（我是学生一）
+         */
+        //获取随机手机号
+        ArrayList<String> mobileList = studentRequestUtil.loadMobileList(count, "171");
+        for (int i = 0; i < mobileList.size(); i++) {
+            String realName = studentRegisterBody.getReal_name();
+            // 检查学生姓名
+            realName += numChars[i];
+            Result<Object> checkSignupObject = studentRequestUtil.checkSignupRealName(realName, Long.toString(clazzId), Long.toString(teacherId));
+            if (checkSignupObject.isSuccess()) {
+                // 如果姓名校验结果为false
+                if (!JSONObject.parseObject(String.valueOf(checkSignupObject.getData())).getString("result").equals("success"))
+                    break;
+                // 判断手机号是否有效 "student_mobile_binded": true  "parent_mobile_binded": false,
+                // 检查手机号是否绑定
+//                Result<Object> mobileBind = studentRequestUtil.requestSignupMobileCheck(Long.toString(teacherId), mobileList.get(i));
+//                if (mobileBind != null) {
+//                    if (mobileBind.isSuccess()) {
+//                        // 判断请求状态
+//                        JSONObject jsonObject = (JSONObject) JSONObject.toJSON(mobileBind.getData());
+//                        String result = jsonObject.getString("result");
+//                        if (!result.equals("success")) {
+//                            break;
+//                        }
+//                        Boolean binded = jsonObject.getBoolean("student_mobile_binded");
+//                        if (binded) break;
+//                    }
+//                }
+                // 发送验证码
+                Result<Object> smsignResult = studentRequestUtil.signupSmsignSvc(mobileList.get(i), Long.toString(teacherId));
+                if (smsignResult.isSuccess()) {
+                    if (!JSONObject.parseObject(String.valueOf(smsignResult.getData())).getString("result").equals("success"))
+                        break;
+                }
+                // 注册
+                Map<String, String> paramMap = BeanUtils.describe(studentRegisterBody);
+                paramMap = studentRequestUtil.signupRegisterParamToMap(paramMap, realName, mobileList.get(i), passwd);
+                Result<Object> registerResult = studentRequestUtil.signupUser(paramMap);
+
+                // 判断注册结果，组装返回
+                if (registerResult.isSuccess()) {
+                    JSONObject response = JSONObject.parseObject(String.valueOf(registerResult.getData()));
+                    if (response == null || !response.getString("result").equals("success")) break;
+//                    sessionKey = jsonObject.getString("session_key");
+                    System.out.println(response.getString("session_key"));
+                    RegisterStudentResponse registerStudentResponse = new RegisterStudentResponse();
+                    registerStudentResponse.setPassword(passwd);
+                    registerStudentResponse.setMobile(mobileList.get(i));
+                    registerStudentResponse.setRealName(realName);
+                    registerStudentResponse.setUserId(response.getLong("user_id"));
+                    registerStudentResponseList.add(registerStudentResponse);
+                    // 检查是否允许加入班级
+//                Result<Object> joinSysResult = studentRequestUtil.requestSignupJoinSystemClazz(Long.toString(clazzId), Long.toString(teacherId));
+                }
+            }
+        }
+        return Result.success(registerStudentResponseList);
+    }
 }
